3c5d727c9ee10a10ef843ef3f0d53f81454c37c5
said kabene <saidkabene@euniv-paris1.com>
2021-01-28 21:37:33 +0100

    création database 23/01/2021

---
+++ website-skeleton/src/Controller/AccueilController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class   AccueilController extends AbstractController
+{
+    /**
+     * @Route("/accueil", name="accueil")
+     */
+    public function index(): Response
+    {
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(){
+        return $this->render('accueil/home.html.twig');
+
+    }
+}
